ef5dfe91f857afa93d7a73434feb770a41f8712e
Garrison Jensen <garrison.jensen@gmail.com>
2014-09-01 09:51:27 -0700

    What are you a n00b?

--- runaway.hs
+++ runaway.hs
@@ -21,18 +21,20 @@ async action = do
 wait :: Async a -> IO a
 wait (Async var) = readMVar var
 
+remoteCheck :: [Char] -> IO ()
 remoteCheck host = do
      procs <- check host 
-     runaways <- return $ case procs of
+     let badProcs = case procs of
                           [] -> []
                           a  -> filterBadProcs $ tail $ lines a
-     runaways <- return $ map ((++) (hostname ++ spaces)) runaways
-     mapM_ print (runaways)
+         runaways = map ((++) (hostname ++ spaces)) badProcs
+     mapM_ print runaways
      return ()
     where
      hostname = (takeWhile (/= '.') host) 
      spaces = replicate (14-(length hostname)) ' '
 
+check :: String -> IO String
 check host = do
     (_, procs, _) <- readProcessWithExitCode  "ssh" ["-oStrictHostKeyChecking=no", 
                                                     "-o ConnectTimeout=5", 
@@ -41,8 +43,10 @@ check host = do
                                                     "user,ni,pid,ppid,pcpu,pmem,time,comm"] []
     return procs
 
+filterBadProcs :: [[Char]] -> [[Char]]
 filterBadProcs l = [bad | bad <- l, isBadProc bad]
 
+isBadProc :: String -> Bool
 isBadProc potentialRunaway = ((highMem) || (highCpu)) && (runningForAwhile) && (isntNice) && (notRoot) && notKnownUser
   where 
     (user:ni:pid:ppid:pcpu:pmem:time:com) = words potentialRunaway
@@ -53,10 +57,12 @@ isBadProc potentialRunaway = ((highMem) || (highCpu)) && (runningForAwhile) && (
     notRoot = user /= "root"
     notKnownUser = not $ elem user knownUsers
 
+hosts :: IO [String]
 hosts = do 
     str <- readProcess "netgrouplist" groupsToCheck []
     return (nub $ lines str)
 
+main :: IO ()
 main = do 
     putStrLn header
     hostlist <- hosts
